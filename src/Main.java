d58844b4618e294cedd6161358a3528fbef73f1c
Denis Petrovskii <denpetrovsky369@gmail.com>
2021-12-22 05:10:36 +0300

    frmiles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 12051;
+        int bonus = 20;
+        int frmiles = price / bonus;
+        System.out.println(frmiles);
+    }
+}
