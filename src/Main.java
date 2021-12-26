def5e1e46fe6c213d3ebbb9b858ab5241b612f01
Denis Petrovskii <denpetrovsky369@gmail.com>
2021-12-26 12:16:08 +0300

    vashimili

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int price = 12051;
+        int price = 14000;
         int bonus = 20;
         int frmiles = price / bonus;
-        System.out.println(frmiles);
+        System.out.println("Вы заработали " + frmiles + " бесплатных миль");
     }
 }
